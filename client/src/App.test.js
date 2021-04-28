b17bac863171ea32dbedfc9ffa3af30fdeec8988
ShirGanon <45141251+ShirGanon@users.noreply.github.com>
2021-04-28 23:03:36 +0300

    unit test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, cleanup, waitFor } from "@testing-library/react";
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow, configure, mount } from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Main from "./components/Main"
+
+configure({ adapter: new Adapter() });
+
+describe("App", () => {
+
+
+  it("loading test", () => {
+    const {getByText} = render(<Main/>);
+    expect(getByText(/Loading/i)).toBeInTheDocument();
+    cleanup();
+  })
+
+
+
+  it("api test", async ()=>{
+    const {getByTestId} = render(<Main/>);
+    const div = await waitFor(() => getByTestId('contactsTest'));
+    expect(div).toBeInTheDocument();
+  })
+
+
+  it("api test2", async () => {
+    const wrapper =  await mount(<Main/>);
+
+    await wrapper.update();
+    console.log(wrapper.debug());
+    expect(wrapper.find("contactsTest").length).toEqual(1);
+    
+  });
+
 });
+
+
+
--- client/src/components/InfoScreen.jsx
+++ client/src/components/InfoScreen.jsx
@@ -6,17 +6,17 @@ export default function InfoScreen(props) {
     let location = selectedUser.location;
     location = `${location.street.name} ${location.street.number}, ${location.city}, ${location.state}, ${location.postcode}`;
     const name = `${selectedUser.name.first}  ${selectedUser.name.last}`;
-    
+
     return (
         <div>
             <div className="infoHeader header">
                 <div className="backButtonDiv" onClick={onBackClick}>
-                    <img src={icon}/>
+                    <img src={icon} alt="back"/>
                     <button className="backButton">Back</button>
                 </div>
                 <h3 className="infoHeaderTitle">{name}</h3>
             </div>
-            <div className="infoImageDiv"><img src={selectedUser.picture.medium}/></div>
+            <div className="infoImageDiv"><img src={selectedUser.picture.medium} alt="contact"/></div>
             <div>
                 <p>{name}</p>
                 <p>{location}</p>
